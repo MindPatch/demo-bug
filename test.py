42f6495cad6320a0b929cf96d13f8c4fe5c7d7b7
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2024-07-02 05:23:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+import os
+TOKEN = os.getenv("OPS_REMOBED")
